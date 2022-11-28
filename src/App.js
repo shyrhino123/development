617e856101d25e4efbf7838f7cf9d8023e387988
gracechenn <gracechen567@gmail.com>
2022-11-28 14:40:08 -0500

    multiple filter

--- src/App.js
+++ src/App.js
@@ -14,62 +14,100 @@ function App() {
   const[cartTotal, setTotal] = useState(0);
   const season = ["All", "summer", "fall", "winter", "spring"];
   const type = ["All", "berry", "tropical", "other"];
+  const sort = ["Default", "Price"];
   function getFruits() {
     const fruitsList = fruitsData;
     return fruitsList;
   }
   
-  function filterSeason(season) {
-    let filteredFruits = getFruits().filter(item => item.season === season);
+  function filterSeason(fruits, season) {
+    let filteredFruits = fruits.filter(item => item.season === season);
     return filteredFruits;
   }
 
-  function filterType(type) {
-    let filteredFruits = getFruits().filter(fruit => fruit.type === type);
+  function filterType(fruits, type) {
+    let filteredFruits = fruits.filter(fruit => fruit.type === type);
     return filteredFruits;
   }
 
 
   const [filterBySeason, setSeasonFilter] = useState(null)
   const [filterByType, setTypeFilter] = useState(null)
+  const [filteredFruits, setFilters] = useState(getFruits());
   useEffect(() => {
-    setSeasonFilter(getFruits());
-  }, []);
-  useEffect(() => {
-    setTypeFilter(getFruits());
-  }, []);
-  // const [filteredFruits, setFilters] = useState(getFruits());
-  // useEffect(() => {
-  //   const seasonFiltered = setSeasonFilter(getFruits());
-  //   const typeFiltered = setTypeFilter(seasonFiltered);
-  //   setFilters(typeFiltered)
-  // }, [filterBySeason, filterByType]);
+    const seasonFiltered = returnSeasonFiltered(getFruits());
+    const typeFiltered = returnTypeFiltered(seasonFiltered);
+    setFilters(typeFiltered)
+  }, [filterBySeason, filterByType]);
 
   function handleSeasons(e) {
     let typeSeason = e.target.value;
-    console.log(typeSeason)
-    typeSeason !== "All"
-      ? setSeasonFilter(filterSeason(typeSeason))
-      : setSeasonFilter(getFruits());
-    console.log(filterBySeason)
-    return filterBySeason;
+    setSeasonFilter(typeSeason);
   }
-
   function handleType(e) {
     let type = e.target.value;
-    console.log(type)
-    type !== "All"
-      ? setTypeFilter(filterType(type))
-      : setTypeFilter(getFruits());
-    console.log(filterByType)
-    return filterByType;
+    setTypeFilter(type);
   }
 
+  function returnSeasonFiltered(fruits) {
+    let filtered = [];
+    if (filterBySeason == null) {
+      return fruits;
+    }
+    filterBySeason !== "All"
+        ? filtered = filterSeason(fruits, filterBySeason)
+        : filtered = fruits
+      console.log(filterBySeason)
+      console.log(filtered)
+      return filtered;
+  }
+  function returnTypeFiltered(fruits) {
+    let filtered = [];
+    if (filterByType == null) {
+      return fruits;
+    }
+    filterByType !== "All"
+        ? filtered = filterType(fruits, filterByType)
+        : filtered = fruits
+      console.log(filterByType)
+      console.log(filtered)
+      return filtered;
+  }
+
+  // const [filterBySeason, setSeasonFilter] = useState(null)
+  // const [filterByType, setTypeFilter] = useState(null)
+  // useEffect(() => {
+  //   setSeasonFilter(getFruits());
+  // }, []);
+  // useEffect(() => {
+  //   setTypeFilter(getFruits());
+  // }, []);
+
+  // function handleSeasons(e) {
+  //   let typeSeason = e.target.value;
+  //   console.log(typeSeason)
+  //   typeSeason !== "All"
+  //     ? setSeasonFilter(filterSeason(typeSeason))
+  //     : setSeasonFilter(getFruits());
+  //   console.log(filterBySeason)
+  //   return filterBySeason;
+  // }
+
+  // function handleType(e) {
+  //   let type = e.target.value;
+  //   console.log(type)
+  //   type !== "All"
+  //     ? setTypeFilter(filterType(type))
+  //     : setTypeFilter(getFruits());
+  //   console.log(filterByType)
+  //   return filterByType;
+  // }
+
   
-  const fruits = filterByType && filterByType.map((item) => (
+  const fruits = filteredFruits && filteredFruits.map((item) => (
     <Card
     style={{
-      width: 300,
+      width: 200,
       margin: 20,
       padding: 0,
     }}
@@ -113,48 +151,53 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Fruit Market</h1>
-        <h2>Filters</h2> 
-        <h3> Sort by </h3>
-        <Radio.Group onChange={onChange} value={value}>
-          <Radio value={1}>Default</Radio>
-          <Radio value={2}>Popularity</Radio>
-          <Radio value={3}>Price</Radio>
-        </Radio.Group>
-        <h3>Seasons</h3>
-        {season &&
-        season.map((s, index) => (
-          <div>
-            <button key={index} value={s} onClick={handleSeasons}>
-              {s}
-            </button>
-          </div>
-        ))}
-  
-        <h3>Type of Fruit</h3>
-        {type &&
-        type.map((s, index) => (
-          <div>
-            <button key={index} value={s} onClick={handleType}>
-              {s}
-            </button>
-          </div>
-        ))}
-
-        <h3> Other </h3>
-        <Checkbox onChange={onChange}>In Cart</Checkbox>
       </header>
-      <div id="grid"> 
-      <Space wrap> 
-        {fruits}
-      </Space>
-      <div id="cart">
+      <div id="body">
+        <div id="filters">
+        <h2>Filters</h2> 
+          <h3> Sort by </h3>
+          {sort &&
+          sort.map((s, index) => (
+            <div>
+              <div>
+              <input type="radio" id={s} name="sort" value={s} onClick={handleType}/>
+              <label for={s}>{s}</label>
+            </div>
+            </div>
+          ))}
 
+          <h3>Seasons</h3>
+          {season &&
+          season.map((s, index) => (
+            <div>
+              <input type="radio" id={s} name="season" value={s} onClick={handleSeasons}/>
+              <label for={s}>{s}</label>
+            </div>
+          ))}
+    
+          <h3>Type of Fruit</h3>
+          {type &&
+          type.map((s, index) => (
+            <div>
+              <div>
+              <input type="radio" id={s} name="type" value={s} onClick={handleType}/>
+              <label for={s}>{s}</label>
+            </div>
+            </div>
+          ))}
+        </div>
+        <div id="grid"> 
+          <Space wrap> 
+            {fruits}
+          </Space>
+        </div>
+      </div> 
+      <div id="cart">
         <h2>Cart</h2>
         {
           Array.from(cart, ([item, count]) => <p>{count} x {item}</p>)
         }
         Total: {cartTotal}
-      </div>
       </div>
     </div>
   );
